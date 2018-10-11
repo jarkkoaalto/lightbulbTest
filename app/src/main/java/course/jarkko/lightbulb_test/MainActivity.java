526c2d1b3a70eb791a164bd5a3f7ed00c236f031
Jarkko Aalto <jarkko.aalto@live.fi>
2018-10-11 20:41:25 +0300

    Added a app layout and MainActivity.java class

--- app/src/main/java/course/jarkko/lightbulb_test/MainActivity.java
+++ app/src/main/java/course/jarkko/lightbulb_test/MainActivity.java
@@ -1,13 +1,43 @@
 package course.jarkko.lightbulb_test;
 
+import android.graphics.drawable.TransitionDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button;
+    boolean turnedOn = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.on_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!turnedOn) {
+                    imageView.setImageResource(R.drawable.trans_on);
+                    ((TransitionDrawable) imageView.getDrawable()).startTransition(3000);
+                    turnedOn = true;
+                } else {
+                    imageView.setImageResource(R.drawable.trans_off);
+                    ((TransitionDrawable) imageView.getDrawable()).startTransition(3000);
+                    turnedOn = false;
+                }
+            }
+        });
+
     }
+
 }
